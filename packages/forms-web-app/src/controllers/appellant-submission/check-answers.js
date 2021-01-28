c90081df5818ae9d5ea36c19094d0fb23455a290
Christopher Moss <chris@a6software.co.uk>
2021-01-28 08:33:11 +0000

    feat(forms-web-app): adds check your answers

--- packages/forms-web-app/src/controllers/appellant-submission/check-answers.js
+++ packages/forms-web-app/src/controllers/appellant-submission/check-answers.js
@@ -1,6 +1,32 @@
 const { getDepartmentFromId } = require('../../services/department.service');
 const { VIEW } = require('../../lib/views');
 
+const isOriginalApplicantFn = (appeal) => {
+  if (
+    !appeal ||
+    !appeal.aboutYouSection ||
+    !appeal.aboutYouSection.yourDetails ||
+    !appeal.aboutYouSection.yourDetails.isOriginalApplicant
+  ) {
+    return null;
+  }
+
+  if (typeof appeal.aboutYouSection.yourDetails.isOriginalApplicant !== 'boolean') {
+    return null;
+  }
+
+  return appeal.aboutYouSection.yourDetails.isOriginalApplicant ? 'Yes' : 'No';
+};
+
+const multifileUploadFileNamesOnly = (multifileUploadsArray) =>
+  multifileUploadsArray.reduce((acc, file) => {
+    if (!file.originalFileName) {
+      return acc;
+    }
+
+    return [...acc, file.originalFileName];
+  }, []);
+
 exports.getCheckAnswers = async (req, res) => {
   const { appeal } = req.session;
   let appealLPD = '';
@@ -15,5 +41,9 @@ exports.getCheckAnswers = async (req, res) => {
   res.render(VIEW.APPELLANT_SUBMISSION.CHECK_ANSWERS, {
     appealLPD,
     appeal,
+    isOriginalApplicant: isOriginalApplicantFn(appeal),
+    otherDocuments: multifileUploadFileNamesOnly(
+      appeal.yourAppealSection.otherDocuments.uploadedFiles
+    ),
   });
 };
